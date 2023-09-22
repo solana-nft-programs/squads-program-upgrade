3764dd0c47018da5ab399d51deff732bc4a552af
Jwalin <jwalin12@gmail.com>
2023-09-22 13:49:37 -0400

    debug logs

--- src/createProgramUpgrade.ts
+++ src/createProgramUpgrade.ts
@@ -1,13 +1,13 @@
-import {AnchorProvider, BN, Program, Wallet} from '@coral-xyz/anchor'
-import {ProgramManager} from './idl/program_manager'
+import { AnchorProvider, BN, Program, Wallet } from '@coral-xyz/anchor'
+import { ProgramManager } from './idl/program_manager'
 import programManager from './idl/program_manager.json'
-import {Connection, Keypair, PublicKey, SystemProgram} from '@solana/web3.js'
+import { Connection, Keypair, PublicKey, SystemProgram } from '@solana/web3.js'
 import {
   getManagedProgramPDA,
   getProgramManagerPDA,
   getProgramUpgradePDA
 } from './pda'
-import {programManagerProgramId} from './constants'
+import { programManagerProgramId } from './constants'
 
 export const createProgramUpgrade = async ({
   multisig,
@@ -44,11 +44,14 @@ export const createProgramUpgrade = async ({
     multisig,
     programManagerProgramId
   )
+  console.log(`program manager pda: ${programManagerPDA.toString()}`);
+  console.log(`program index: ${programIndex}`);
   const [managedProgramPDA] = await getManagedProgramPDA(
     programManagerPDA,
     new BN(programIndex),
     programManagerProgramId
   )
+  console.log(`managed program pda: ${managedProgramPDA.toString()}`);
   const managedProgram = await program.account.managedProgram.fetch(
     managedProgramPDA
   )
