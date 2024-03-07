1d46abd47dfae5aa6ad97e341b13693c3d2ff092
Bhargava Sai Macha <mbsai29@gmail.com>
2024-03-06 20:15:13 -0500

    prio fee

--- src/createProgramUpgrade.ts
+++ src/createProgramUpgrade.ts
@@ -1,13 +1,19 @@
-import { AnchorProvider, BN, Program, Wallet } from '@coral-xyz/anchor'
-import { ProgramManager } from './idl/program_manager'
+import {AnchorProvider, BN, Program, Wallet} from '@coral-xyz/anchor'
+import {ProgramManager} from './idl/program_manager'
 import programManager from './idl/program_manager.json'
-import { Connection, Keypair, PublicKey, SystemProgram } from '@solana/web3.js'
+import {
+  ComputeBudgetProgram,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram
+} from '@solana/web3.js'
 import {
   getManagedProgramPDA,
   getProgramManagerPDA,
   getProgramUpgradePDA
 } from './pda'
-import { programManagerProgramId } from './constants'
+import {programManagerProgramId} from './constants'
 
 export const createProgramUpgrade = async ({
   multisig,
@@ -44,14 +50,14 @@ export const createProgramUpgrade = async ({
     multisig,
     programManagerProgramId
   )
-  console.log(`program manager pda: ${programManagerPDA.toString()}`);
-  console.log(`program index: ${programIndex}`);
+  console.log(`program manager pda: ${programManagerPDA.toString()}`)
+  console.log(`program index: ${programIndex}`)
   const [managedProgramPDA] = await getManagedProgramPDA(
     programManagerPDA,
     new BN(programIndex),
     programManagerProgramId
   )
-  console.log(`managed program pda: ${managedProgramPDA.toString()}`);
+  console.log(`managed program pda: ${managedProgramPDA.toString()}`)
   const managedProgram = await program.account.managedProgram.fetch(
     managedProgramPDA
   )
@@ -79,6 +85,11 @@ export const createProgramUpgrade = async ({
       programUpgrade: programUpgradePDA,
       systemProgram: SystemProgram.programId
     })
+    .preInstructions([
+      ComputeBudgetProgram.setComputeUnitPrice({
+        microLamports: new BN(1000)
+      })
+    ])
   const txid = await methods.rpc()
   console.log(
     `Successfully created program upgrade for MS_PDA ${multisig.toString()} https://explorer.solana.com/tx/${txid}`
